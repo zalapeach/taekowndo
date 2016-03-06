c30b5e9d83ab4a5cd1c7a62359795dc0b7abbda7
Juan Carlos <zalapeach@gmail.com>
2016-03-06 16:52:37 -0600

    adds model student

---
+++ spec/factories/students.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :student do
+    danbom "MyString"
+    name "MyString"
+    age 1
+    state "MyString"
+    dojang "MyString"
+    teacher "MyString"
+    exam_age "MyString"
+  end
+end
