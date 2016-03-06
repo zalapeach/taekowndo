2253aa69b75a4dc65fa05b5b35d8c3f03e5c43e3
Juan Carlos <zalapeach@gmail.com>
2016-03-06 16:53:06 -0600

    created admin students controller

---
+++ app/controllers/admin/students_controller.rb
@@ -0,0 +1,51 @@
+class Admin::StudentsController < Admin::BaseController
+  def index
+    @students = Student.all
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      redirect_to admin_students_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    if @student.update(student_params)
+      redirect_to admin_students_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @student = Student.find(params[:id])
+    @student.destroy
+    redirect_to admin_students_path
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(
+      :danbom,
+      :name,
+      :age,
+      :state,
+      :dojang,
+      :teacher,
+      :exam_age
+    )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root 'home#index'
+    resources :students
   end
 end
