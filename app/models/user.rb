a30d01ec5f82ad00c9223a72022973bcb8ce7831
Juan Carlos <zalapeach@gmail.com>
2016-04-05 20:45:14 -0500

    Adds errors field and locale to spanish

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+
   def self.omniauth_auth(omniauth_info)
     data = omniauth_info.info
     user = User.where(email: data[:email]).first
