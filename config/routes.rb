54a6f92b98a9f879988a2119cb38948974d0bba1
Juan Carlos <zalapeach@gmail.com>
2016-06-19 15:36:57 -0500

    Creates show student for public and admin sites

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'logout', to: 'sessions#destroy', as: 'logout'
+  resources :students, only: :show
 
   resources :sessions, only: [:create, :destroy]
 
