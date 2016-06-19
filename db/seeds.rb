ff5769eb34c137126c03d0ebbc54aec1797fbfef
Juan Carlos <zalapeach@gmail.com>
2016-06-19 12:29:48 -0500

    Add more taekwondo grades to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,9 @@ User.create!(email: 'zalapeach@gmail.com')
 
 [
   '1er Dan', '2do Dan', '3er Dan', '4to Dan', '5to Dan', '6to Dan', '7mo Dan',
-  '8vo Dan', '9no Dan', '1er Poom', '2do Poom', '3er Poom'
+  '8vo Dan', '9no Dan', '1er Poom', '2do Poom', '3er Poom', '10mo Kup',
+  '9no Kup', '8vo Kup', '7mo Kup', '6to Kup', '5to Kup', '4to Kup', '3er Kup',
+  '2do Kup', '1er Kup'
 ].each do |grade|
   Grade.create!(name: grade)
 end
